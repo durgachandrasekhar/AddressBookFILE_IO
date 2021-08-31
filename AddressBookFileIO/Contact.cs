fdfb98ccf9d593b8225a891dbbad2dee383abafe
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-31 09:15:56 +0530

    Sort by Person name

---
+++ AddressBookFileIO/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class Contact
+    {
+        public Contact(string firstname, string lastname, string address, string city, string state, string zipcode, string phonenumber, string email)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.ZipCode = zipcode;
+            this.PhoneNumber = phonenumber;
+            this.Email = email;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ AddressBookFileIO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to Address Book.");
+            Console.WriteLine("=================================\nChoose the operation:\n1.Add Contact.\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Duplicate Checker.\n6.Search Person.\n7.Find City/State.\n8.Find Number Of Persons..\n9.Sort By Person Name\n10.Exit Adress Book Application.\n=================================");
+            var userInput = Console.ReadLine();
+            var addressBook = new AddressBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        addressBook.StoreContact();
+                        break;
+                    case "2":
+                        addressBook.DisplayAllContact();
+                        break;
+                    case "3":
+                        addressBook.EditContact();
+                        break;
+                    case "4":
+                        addressBook.DeleteContact();
+                        break;
+                    case "5":
+                        addressBook.DuplicateChecker();
+                        break;
+                    case "6":
+                        addressBook.SearchPerson();
+                        break;
+                    case "7":
+                        addressBook.FindCityState();
+                        break;
+                    case "8":
+                        addressBook.FindNumOfPerson();
+                        break;
+                    case "9":
+                        addressBook.SortByPersonName();
+                        break;
+                    case "10":
+                        return;
+                    default:
+                        Console.WriteLine("!!! Choose valid operation !!!");
+                        break;
+                }
+                Console.WriteLine("=================================\nChoose the operation:\n1.Add Another Contact.\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Duplicate Checker.\n6.Search Person.\n7.FindCity/State.\n8.Find Number Of Persons..\n9.Sort By Person Name.\n10.Exit Address Book Application.\n=================================");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
